25bba279c98db1632288595c08ab715facea0808
jennifer <cxxyao2@gmail.com>
2023-02-12 14:08:02 -0500

    add mutation

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,16 +1,46 @@
 import express from 'express'
 import * as trpc from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
+import cors from 'cors'
+import { z } from 'zod'
 
-const appRouter = trpc.router().query('hello', {
-	resolve() {
-		return 'Hello World'
-	}
-})
+interface ChatMessage {
+	user: string
+	message: string
+}
+
+const messages: ChatMessage[] = [
+	{ user: 'user1', message: 'Hello' },
+	{ user: 'user2', message: 'Hi' }
+]
+
+const appRouter = trpc
+	.router()
+	.query('hello', {
+		resolve({ ctx }) {
+			return 'Hello World 2'
+		}
+	})
+	.query('getMessages', {
+		input: z.number().default(10),
+		resolve({ input }) {
+			return messages.slice(-input)
+		}
+	})
+	.mutation('addMessage', {
+		input: z.object({ user: z.string(), message: z.string() }),
+		resolve({ input }) {
+			messages.push(input)
+			return input
+		}
+	})
+
+export type AppRouter = typeof appRouter
 
 const app = express()
+app.use(cors())
 const port = 8080
-
+// In context, we can add authentication etc.
 app.use(
 	'/trpc',
 	trpcExpress.createExpressMiddleware({
